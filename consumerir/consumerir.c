7ae20be20bdc7d78059e6f7fa7df48589e2e7993
Schischu <schischu65@gmail.com>
2016-04-17 18:00:03 +0200

    Add reversed consumerir src

--- consumerir/consumerir.c
+++ consumerir/consumerir.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2016 Schischu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "ConsumerIrHal"
 
 #include <stdlib.h>
 #include <malloc.h>
@@ -22,151 +22,213 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
+
+#define LOG_TAG "ConsumerIrHal"
+#define LOG_NDEBUG 0
 #include <cutils/log.h>
+
 #include <hardware/hardware.h>
 #include <hardware/consumerir.h>
 
+#define FPGA_IR_PATH "/sys/class/sec/sec_ir/ir_send"
+
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
+#define IR_BUFFER_SIZE 0x1000
+
 static const consumerir_freq_range_t consumerir_freqs[] = {
-    {.min = 30000, .max = 30000},
-    {.min = 33000, .max = 33000},
-    {.min = 36000, .max = 36000},
-    {.min = 38000, .max = 38000},
-    {.min = 40000, .max = 40000},
-    {.min = 56000, .max = 56000},
+    {.min = 16000, .max = 60000},
 };
 
-static bool
-try_append_number(char *buffer, int *len, int size, int number)
+static char* SEPARATOR_EOF = "\0";
+static char* SEPARATOR_COMMA = ",";
+
+static int g_sol = -1;
+static pthread_mutex_t g_mutex   = PTHREAD_MUTEX_INITIALIZER;
+
+/* OK 03/04/2016 */
+static int consumerir_transmit_internal(int carrier_freq, int pattern[], int pattern_len)
 {
-    int stored;
+    int i;
+    char buffer[IR_BUFFER_SIZE];
+    int buffer_len;
+    char tmp_buffer[80];
+    char *separator;
+    char path[4096];
+    int fd;
+    struct timespec start_time;
+    struct timespec current_time;
 
-    stored = snprintf(&buffer[*len], size - *len, "%d,", number);
-
-    if (stored < 0 || stored >= size - *len) {
-        return false;
+    snprintf(buffer, IR_BUFFER_SIZE, "%d", carrier_freq);
+    
+    for (i = 0; i < pattern_len; i++)
+    {
+        if (i == pattern_len - 1)
+        {
+            separator = SEPARATOR_EOF;
+        }
+        else
+        {
+            separator = SEPARATOR_COMMA;
+        }
+        
+        snprintf(tmp_buffer, 80, "%d%s", pattern[i], separator);
+        
+        if (strlcat(buffer, tmp_buffer, 4096) >= 4096)
+        {
+          ALOGE("Error: too many pattern\n");
+          return -E2BIG;
+        }
     }
+    
+    strcpy(path, FPGA_IR_PATH);
 
-    *len += stored;
-    return true;
+    clock_gettime(1, &start_time);
+    
+    start_time.tv_sec += (start_time.tv_nsec + 100000000) / 1000000000;
+    start_time.tv_nsec = (start_time.tv_nsec + 100000000) % 1000000000;
+    
+    for (i = 0; i < 10; i++)
+    {
+        fd = open(FPGA_IR_PATH, O_RDWR);
+        
+        if (fd >= 0)
+        {
+            break;
+        }
+        
+        if (errno != EINTR && errno != EACCES)
+        {
+            clock_gettime(1, &current_time);
+            if (current_time.tv_sec >= start_time.tv_sec &&
+                current_time.tv_nsec >= start_time.tv_nsec)
+            {
+                ALOGE("Failed to open! (err=%d)", -errno);
+                return -errno;
+            }
+        }
+    }
+    
+    buffer_len = strlen(buffer);
+    
+    ALOGV("%d - %s", buffer_len, buffer);
+    
+    if (write(fd, buffer, buffer_len) >= 0)
+    {
+        close(fd);
+        return 0;
+    }
+    
+    close(fd);
+    
+    return -errno;
 }
 
-static bool
-grow_buffer(char **buffer, int *size)
-{
-    char *new_buffer;
-
-    *size *= 2;
-    if ((new_buffer = realloc(*buffer, *size)) == NULL) {
-        return false;
-    }
-    *buffer = new_buffer;
-    return true;
-}
-
-static bool
-append_number(char **buffer, int *len, int *size, int number)
-{
-    if (! try_append_number(*buffer, len, *size, number)) {
-        if (! grow_buffer(buffer, size)) return false;
-        return try_append_number(*buffer, len, *size, number);
-    } else {
-        return true;
-    }
-}
-
-int fd = 0;
+/* OK 03/04/2016 */
 static int consumerir_transmit(struct consumerir_device *dev,
    int carrier_freq, int pattern[], int pattern_len)
 {
-    int buffer_len = 0;
-    int buffer_size = 128;
-    int i;
-    char *buffer;
+    int ret;
 
-    if ((buffer = malloc(buffer_size)) == NULL) {
-        return -ENOMEM;
-    }
+    ALOGD("Consumer IR HAL is going to transmit - Type : %d", g_sol);
 
-    /* write the header */
-    if (! append_number(&buffer, &buffer_len, &buffer_size, carrier_freq)) {
-        goto error;
-    }
-
-    /* calculate factor of conversion from microseconds to pulses */
-    float factor = 1000000 / carrier_freq;
-
-    /* write out the timing pattern */
-    for (i = 0; i < pattern_len; i++)
+    pthread_mutex_lock(&g_mutex);
+    
+    ret = consumerir_transmit_internal(carrier_freq, pattern, pattern_len);
+    
+    if (ret < 0)
     {
-        if (! append_number(&buffer, &buffer_len, &buffer_size, (int) (pattern[i]/factor))) {
-            goto error;
-        }
+        ALOGE("Consumer IR Transmit Failed. - CIR (err = %d)", ret);
     }
 
-    buffer[buffer_len - 1] = 0;
-    write(fd, buffer, buffer_len - 1);
-
-    free(buffer);
-
-    return 0;
-
-error:
-    free(buffer);
-    return -ENOMEM;
+    pthread_mutex_unlock(&g_mutex);
+    
+    return ret;
 }
 
+/* OK 03/04/2016 */
 static int consumerir_get_num_carrier_freqs(struct consumerir_device *dev)
 {
     return ARRAY_SIZE(consumerir_freqs);
 }
 
+/* OK 03/04/2016 */
 static int consumerir_get_carrier_freqs(struct consumerir_device *dev,
     size_t len, consumerir_freq_range_t *ranges)
 {
     size_t to_copy = ARRAY_SIZE(consumerir_freqs);
 
     to_copy = len < to_copy ? len : to_copy;
+
     memcpy(ranges, consumerir_freqs, to_copy * sizeof(consumerir_freq_range_t));
     return to_copy;
 }
 
+/* OK 03/04/2016 */
 static int consumerir_close(hw_device_t *dev)
 {
     free(dev);
-    close(fd);
     return 0;
 }
 
-/*
- * Generic device handling
- */
+/* OK 03/04/2016 */
 static int consumerir_open(const hw_module_t* module, const char* name,
         hw_device_t** device)
 {
-    if (strcmp(name, CONSUMERIR_TRANSMITTER) != 0) {
+    /* OK 03/04/2016 */
+    if (strcmp(name, CONSUMERIR_TRANSMITTER) != 0)
+    {
         return -EINVAL;
     }
-    if (device == NULL) {
+
+    /* OK 03/04/2016 */
+    if (device == NULL)
+    {
         ALOGE("NULL device on open");
         return -EINVAL;
     }
 
+    if (g_sol < 0)
+    {
+        int fd;
+
+        ALOGD("Try to use F Chip.");
+
+        fd = open(FPGA_IR_PATH, O_RDWR);
+        if (fd < 0)
+        {
+            ALOGE("Consumer IR HAL is failed to load (F Chip)");
+        }
+
+        g_sol = 0;
+        
+        if (close(fd) < 0)
+        {
+            ALOGE("Fail to close fd : F");
+        }
+    }
+
+    if (g_sol < 0)
+    {
+        ALOGE("Consumer IR Hal is failed to load !");
+    }
+
+    ALOGD("Consumer IR HAL is going to be loaded [Type : %s] [SOL : %d]", "TIME", g_sol);
+
     consumerir_device_t *dev = malloc(sizeof(consumerir_device_t));
     memset(dev, 0, sizeof(consumerir_device_t));
 
-    dev->common.tag = HARDWARE_DEVICE_TAG;
-    dev->common.version = 0;
-    dev->common.module = (struct hw_module_t*) module;
-    dev->common.close = consumerir_close;
+    dev->common.tag            = HARDWARE_DEVICE_TAG;
+    dev->common.version        = 0;
+    dev->common.module         = (struct hw_module_t*) module;
+    dev->common.close          = consumerir_close;
 
-    dev->transmit = consumerir_transmit;
+    dev->transmit              = consumerir_transmit;
     dev->get_num_carrier_freqs = consumerir_get_num_carrier_freqs;
-    dev->get_carrier_freqs = consumerir_get_carrier_freqs;
+    dev->get_carrier_freqs     = consumerir_get_carrier_freqs;
 
     *device = (hw_device_t*) dev;
-    fd = open("/sys/class/sec/sec_ir/ir_send", O_RDWR);
     return 0;
 }
 
@@ -179,9 +241,9 @@ consumerir_module_t HAL_MODULE_INFO_SYM = {
         .tag                = HARDWARE_MODULE_TAG,
         .module_api_version = CONSUMERIR_MODULE_API_VERSION_1_0,
         .hal_api_version    = HARDWARE_HAL_API_VERSION,
-        .id                 = CONSUMERIR_HARDWARE_MODULE_ID,
-        .name               = "Consumer IR Module",
-        .author             = "The CyanogenMod Project",
+        .id                 = CONSUMERIR_HARDWARE_MODULE_ID, /*consumerir*/
+        .name               = "Samsung IR HAL",
+        .author             = "Samsung Electronics",
         .methods            = &consumerir_module_methods,
     },
 };
