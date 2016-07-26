2b485ba621ed7f2b91f63f3261c1478dffaa1fd9
Schischu <schischu65@gmail.com>
2016-07-26 22:01:42 +0200

    Apply gellmars bt audio handsfree bugfix

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -821,6 +821,12 @@ static int do_out_standby(struct stream_out *out)
 
     ALOGV("%s: output standby: %d", __func__, out->standby);
 
+    /* if in-call, dont turn off PCM */
+    if (adev->in_call) {
+        ALOGV("%s: output standby in-call, exiting...", __func__);
+        return 0;
+    }
+
     if (!out->standby) {
         for (i = 0; i < PCM_TOTAL; i++) {
             if (out->pcm[i]) {
@@ -1073,6 +1079,12 @@ static int in_set_format(struct audio_stream *stream, audio_format_t format)
 static int do_in_standby(struct stream_in *in)
 {
     struct audio_device *adev = in->dev;
+
+    /* if in-call, dont turn off PCM */
+    if (adev->in_call) {
+        ALOGV("%s: input standby in-call, exiting...", __func__);
+        return 0;
+    }
 
     if (!in->standby) {
         pcm_close(in->pcm);
