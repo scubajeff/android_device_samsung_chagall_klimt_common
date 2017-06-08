da93ff5cf3397e5391681b7ceb7b258d00da5a04
Jeffrey Liu <scubajeffrey@gmail.com>
2017-06-08 14:33:23 +0800

    updated wifi configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,8 +68,6 @@ BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_NVRAM_PATH_PARAM     := "/sys/module/dhd/parameters/nvram_path"
-WIFI_DRIVER_NVRAM_PATH           := "/system/etc/wifi/nvram_net.txt"
 
 # Camera
 # frameworks/av/services/camera/libcameraservice/Android.mk
